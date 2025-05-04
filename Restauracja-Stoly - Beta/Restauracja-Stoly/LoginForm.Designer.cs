c5ef33c34888e5c0719e4a4122107d577b655c6a
AL2137 <alanloch123@gmail.com>
2025-05-04 23:34:57 +0200

    Dodanie panelu
    Dodanie panelu  logowania do aplikacji

---
+++ Restauracja-Stoly - Beta/Restauracja-Stoly/LoginForm.Designer.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+
+namespace Restauracja_Stoly
+{
+    partial class LoginForm
+    {
+        private void InitializeComponent()
+        {
+            labelUsername = new Label() { Text = "Użytkownik:", Left = 20, Top = 20, Width = 80 };
+            txtusername = new TextBox() { Left = 110, Top = 20, Width = 200 };
+
+            labelPassword = new Label() { Text = "Hasło:", Left = 20, Top = 60, Width = 80 };
+            txtpassword = new TextBox() { Left = 110, Top = 60, Width = 200, PasswordChar = '●' };
+
+            buttonLogin = new Button() { Text = "Zaloguj", Left = 20, Top = 100, Width = 90 };
+            buttonLogin.Click += buttonLogin_Click;
+
+            buttonClear = new Button() { Text = "Wyczyść", Left = 125, Top = 100, Width = 90 };
+            buttonClear.Click += (s, e) => { txtusername.Clear(); txtpassword.Clear(); txtusername.Focus(); };
+
+            buttonExit = new Button() { Text = "Wyjdź", Left = 220, Top = 100, Width = 90 };
+            buttonExit.Click += (s, e) => Application.Exit();
+
+            this.Controls.Add(labelUsername);
+            this.Controls.Add(txtusername);
+            this.Controls.Add(labelPassword);
+            this.Controls.Add(txtpassword);
+            this.Controls.Add(buttonLogin);
+            this.Controls.Add(buttonClear);
+            this.Controls.Add(buttonExit);
+        }
+    }
+}
---
+++ Restauracja-Stoly - Beta/Restauracja-Stoly/LoginForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Restauracja_Stoly
+{
+    public partial class LoginForm : Form
+    {
+        private TextBox txtusername;
+        private TextBox txtpassword;
+        private Button buttonLogin;
+        private Button buttonClear;
+        private Button buttonExit;
+        private Label labelUsername;
+        private Label labelPassword;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            this.Text = "Panel logowania";
+            this.Size = new Size(350, 220);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+       
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (txtusername.Text == "admin123" && txtpassword.Text == "admin123")
+            {
+                MessageBox.Show("Zalogowano pomyslnie!");
+                Form1 form1 = new Form1();
+                form1.Show(); // Show Form1
+                
+
+            }
+            else
+            {
+                MessageBox.Show("Bledne dane logowania");
+                txtusername.Clear();
+                txtpassword.Clear();
+                txtusername.Focus();
+            }
+        }
+    }
+}
